736df61cbea42c23bd328dec2c4361d1cbae9f42
Andrew Galicia <andrewlgalicia@gmail.com>
2023-06-27 13:38:04 -0700

    bug fixes

--- src/components/RightPanel/RightPanel.jsx
+++ src/components/RightPanel/RightPanel.jsx
@@ -7,16 +7,7 @@ import './RightPanel.css';
 export default function RightPanel() {
   return (
     <aside className="right-panel">
-     <Link to="/my-games"> My Games</Link>
-     &nbsp;&nbsp;
-
-     
-     <Link to="/joined-games"> Joined Games</Link>
-
-      <Routes>
-        <Route path="/my-games" element={<MyGames />} /> {/* Use the MyGames component */}
-        <Route path="/joined-games" element={<JoinedGames />} /> {/* Use the JoinedGames component */}
-      </Routes>
+ 
     </aside>
   );
 }
