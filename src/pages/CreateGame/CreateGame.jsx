992fe8d27d32a030541388539b3086d66dc179f9
Andrew Galicia <andrewlgalicia@gmail.com>
2023-06-29 11:14:48 -0700

    push before test create game

--- src/pages/CreateGame/CreateGame.jsx
+++ src/pages/CreateGame/CreateGame.jsx
@@ -1,33 +1,16 @@
 import React, { useState } from 'react';
-import {
-  GoogleMap,
-  useLoadScript,
-  Marker,
-  InfoWindow,
-} from "@react-google-maps/api";
 import usePlacesAutocomplete, {
   getGeocode,
   getLatLng,
-} from "use-places-autocomplete";
+} from 'use-places-autocomplete';
 import {
   Combobox,
   ComboboxInput,
   ComboboxPopover,
   ComboboxList,
   ComboboxOption,
-} from "@reach/combobox";
-import "@reach/combobox/styles.css";
-
-const libraries = ["places"];
-const mapContainerStyle = {
-  width: "60vh",
-  height: "500px",
-};
-
-const center = {
-  lat: 37.8,
-  lng: -122.27
-};
+} from '@reach/combobox';
+import '@reach/combobox/styles.css';
 
 const CreateGame = () => {
   const [title, setTitle] = useState('');
@@ -38,27 +21,7 @@ const CreateGame = () => {
   const handleSelect = async (address) => {
     const results = await getGeocode({ address });
     const { lat, lng } = await getLatLng(results[0]);
-    setSelected({ lat, lng });
     setLocation(address);
-
-    // Fetch nearby fields
-    const service = new window.google.maps.places.PlacesService(
-      document.createElement("div")
-    );
-    const request = {
-      location: { lat, lng },
-      radius: 1000, // Adjust the radius as per your requirement
-      type: "park",
-    };
-    service.nearbySearch(request, (results, status) => {
-      if (status === window.google.maps.places.PlacesServiceStatus.OK) {
-        setNearbyFields(results);
-      }
-    });
-  };
-
-  const handleMarkerClick = (field) => {
-    setSelectedField(field);
   };
 
   const handleSubmit = (e) => {
@@ -75,66 +38,6 @@ const CreateGame = () => {
     // Perform the necessary logic for game creation
     console.log(newGame);
   };
-
-  const renderMap = () => {
-    return (
-      <GoogleMap
-        zoom={11}
-        center={selected ? selected : center}
-        mapContainerStyle={mapContainerStyle}
-      >
-        {selected && <Marker position={selected} />}
-        {nearbyFields.map((field) => (
-          <Marker
-            key={field.place_id}
-            position={{
-              lat: field.geometry.location.lat(),
-              lng: field.geometry.location.lng(),
-            }}
-            icon={{
-              url: "https://maps.google.com/mapfiles/ms/icons/red-dot.png",
-              scaledSize: new window.google.maps.Size(30, 30),
-            }}
-            onClick={() => handleMarkerClick(field)}
-          />
-        ))}
-        {selectedField && (
-          <InfoWindow
-            position={{
-              lat: selectedField.geometry.location.lat(),
-              lng: selectedField.geometry.location.lng(),
-            }}
-            onCloseClick={() => setSelectedField(null)}
-          >
-            <div>
-              <h2>{selectedField.name}</h2>
-              <p>{selectedField.vicinity}</p>
-              {selectedField.photos && selectedField.photos[0] && (
-                <img
-                  src={selectedField.photos[0].getUrl()}
-                  alt={selectedField.name}
-                  style={{ maxWidth: "200px" }}
-                />
-              )}
-            </div>
-          </InfoWindow>
-        )}
-      </GoogleMap>
-    );
-  };
-
-  const { isLoaded, loadError } = useLoadScript({
-    googleMapsApiKey: process.env.REACT_APP_NEXT_PUBLIC_GOOGLE_MAPS_API_KEY,
-    libraries,
-  });
-
-  const [selected, setSelected] = useState(null);
-  const [nearbyFields, setNearbyFields] = useState([]);
-  const [selectedField, setSelectedField] = useState(null);
-
-  if (loadError) {
-    return <div>Error loading maps</div>;
-  }
 
   return (
     <div>
@@ -177,28 +80,20 @@ const CreateGame = () => {
         <br />
         <label>
           Location:
-          <PlacesAutocomplete handleSelect={handleSelect} />
+          <PlacesAutocomplete handleSelect={handleSelect} setValue={setLocation} />
         </label>
         <br />
         <button type="submit">Create Game</button>
       </form>
-      {isLoaded ? (
-        <div>
-          {renderMap()}
-        </div>
-      ) : (
-        <div>Loading...</div>
-      )}
     </div>
   );
 };
 
-const PlacesAutocomplete = ({ handleSelect }) => {
+const PlacesAutocomplete = ({ handleSelect, setValue }) => {
   const {
     ready,
     value,
     suggestions: { status, data },
-    setValue,
   } = usePlacesAutocomplete({
     debounce: 300,
   });
@@ -217,7 +112,7 @@ const PlacesAutocomplete = ({ handleSelect }) => {
       />
       <ComboboxPopover>
         <ComboboxList>
-          {status === "OK" &&
+          {status === 'OK' &&
             data.map(({ place_id, description }) => (
               <ComboboxOption key={place_id} value={description} />
             ))}
