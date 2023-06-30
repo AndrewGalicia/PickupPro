66aa8aaa0c61bc6d346693b2019db714232a50f0
Andrew Galicia <andrewlgalicia@gmail.com>
2023-06-30 14:04:18 -0700

    card set up

--- src/pages/GameCard/GameCard.jsx
+++ src/pages/GameCard/GameCard.jsx
@@ -1,5 +1,16 @@
-export default function GameCard(game) {
-    return (
-        <p>game card working</p>
-    )
+import React from 'react';
+import './GameCard.css'
+
+export default function GameCard({ game }) {
+  const { title, date, skillLevelRequirement, location, participants } = game;
+
+  return (
+    <div className="GameCard">
+      <h2>{title}</h2>
+      <p>Date: {date}</p>
+      <p>Skill Level: {skillLevelRequirement}</p>
+      <p>Location: {location}</p>
+      <p>Participants: {participants.join(', ')}</p>
+    </div>
+  );
 }
